2a1ca53b61bb2bc9be84c45adfd1fb7f92ad497b
Eduardo Praxedes <dupraxedes1990@gmail.com>
2024-09-21 17:10:32 -0300

    commit primeiro arquivo

---
+++ src/App.java
@@ -0,0 +1,11 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        int a, b, soma;
+        a = 10;
+        b = 20;
+        soma = a + b; 
+        System.out.println("A soma do valor de A com B é "+  soma);
+
+    
+    }
+}
---
+++ src/ExercicioEscreverComentar.java
@@ -0,0 +1,11 @@
+public class ExercicioEscreverComentar {
+    public static void main(String[] args) {
+        //Exemplo de comentario em uma linha:  Aqui voce pode escrever o que você quiser
+
+        /*Essa parte é para comentário 
+        Mas a diferença é que esse pode pular linha
+         */
+        System.out.println("Comentários Adcionados");
+    }
+
+}
